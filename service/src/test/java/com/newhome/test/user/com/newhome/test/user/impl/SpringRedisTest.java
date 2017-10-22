6894d1802eec3ddfced4e95ae6f03de78dac6c5c
zhangjiayu <790240736@qq.com>
2017-10-22 16:29:50 +0800

    redis  添加配置  测试成功

--- service/src/test/java/com/newhome/test/user/com/newhome/test/user/impl/SpringRedisTest.java
+++ service/src/test/java/com/newhome/test/user/com/newhome/test/user/impl/SpringRedisTest.java
@@ -3,6 +3,8 @@ package com.newhome.test.user.com.newhome.test.user.impl;
 import com.newhome.test.user.AbstractServiceTest;
 import com.newhome.util.bean.RedisUtil;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 /**
  * 测试使用redis
@@ -12,12 +14,12 @@ import org.junit.Test;
  */
 public class SpringRedisTest extends AbstractServiceTest{
 
+    @Autowired
     private RedisUtil redisUtil;
 
     @Test
     public void testSpringRedis(){
 
-        redisUtil = new RedisUtil();
         redisUtil.set("zhangjiayu","ceshi1");
         System.out.println("插入成功");
         String text = (String) redisUtil.get("zhangjiayu");
---
+++ util/src/main/java/com/newhome/util/bean/JedisPoolUtil.java
@@ -0,0 +1,27 @@
+package com.newhome.util.bean;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+
+/**
+ * Jedis连接池配置
+ *
+ * @author zhangjiayu zhangjiayu
+ * @create 2017/10/22
+ */
+public class JedisPoolUtil extends JedisPool {
+
+    public JedisPoolUtil(final GenericObjectPoolConfig poolConfig, final String host, int port, int timeout,
+                         final String password) {
+        super(poolConfig, host, port, timeout, StringUtils.isNotEmpty(password) ? password : null,
+                Protocol.DEFAULT_DATABASE, null);
+    }
+
+    public JedisPoolUtil(final GenericObjectPoolConfig poolConfig, final String host, int port, int timeout,
+                         final String password, final int database) {
+        super(poolConfig, host, port, timeout, StringUtils.isNotEmpty(password) ? password : null, database, null);
+    }
+
+}
