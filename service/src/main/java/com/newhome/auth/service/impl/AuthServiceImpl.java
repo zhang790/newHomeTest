eba73c3acd3cd2eb64de7fb52b26eddc01a8df78
zhangjiayu <790240736@qq.com>
2017-10-28 10:45:48 +0800

    代码编写   登录注册接口  前后端分离02

--- service/src/main/java/com/newhome/auth/service/impl/AuthServiceImpl.java
+++ service/src/main/java/com/newhome/auth/service/impl/AuthServiceImpl.java
@@ -1,8 +1,13 @@
 package com.newhome.auth.service.impl;
 
+import com.newhome.auth.mapper.UserMapper;
 import com.newhome.auth.model.User;
 import com.newhome.auth.service.AuthService;
 import com.newhome.util.bean.ReturnData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 /**
  * 用户登录校验服务
@@ -12,6 +17,11 @@ import com.newhome.util.bean.ReturnData;
  */
 public class AuthServiceImpl implements AuthService{
 
+    @Autowired
+    private UserMapper userMapper;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public ReturnData authUser(String account, String password) {
 
@@ -19,15 +29,47 @@ public class AuthServiceImpl implements AuthService{
         returnData.setData("");
         returnData.setCode(ReturnData.FAIL);
 
-//        if(){
-//
-//        }
+        logger.info("用户登录掺入参数  account:{},password:{}",account,password);
 
-        return null;
+        if(StringUtils.hasText(account) || StringUtils.hasText(password)){
+            returnData.setMsg("用户名或密码为空");
+            return returnData;
+        }
+
+        //判断用户是否存在
+        String rightPwd = userMapper.selectUserByAccountAndPwd(account);
+        if (password.equals(rightPwd)){
+            returnData.setMsg("账号或密码错误");
+            return returnData;
+        }
+
+        returnData.setCode(ReturnData.SUCCESS);
+        return returnData;
     }
 
     @Override
     public ReturnData insertUser(User user) {
+
+        ReturnData returnData = new ReturnData();
+        returnData.setData("");
+        returnData.setCode(ReturnData.FAIL);
+
+        if(StringUtils.hasText(user.getAccount()) || StringUtils.hasText(user.getPassword())
+        || StringUtils.hasText(user.getEmail()) || StringUtils.hasText(user.getPhone())){
+            returnData.setMsg("注册信息存在空值");
+            return returnData;
+        }
+
+        //保存用户信息
+        try {
+            userMapper.saveUser(user);
+        }catch (Exception ex){
+            //异常处理
+        }
+
+
+
+
         return null;
     }
 }
